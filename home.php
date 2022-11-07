b5cf5bb1ed06ea5a00fb35d02461c9a6705fb792
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-11-07 17:55:55 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -23,49 +23,49 @@
                         <div class="fast-filter">
                             <div class="text-center">
                                 <input class="radio-input " type="checkbox" value="0" id="company" name="type" checked>
-                                <label class="radio-label " for="company"><i class="fa-solid fa-calendar-check"></i></label>
+                                <label class="radio-label " for="company"><i class="fa-solid fa-square-caret-down"></i></label>
                                 <p id="p-company" class="title">Todos</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="person" name="type">
-                                <label class="radio-label " for="person"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="person"><i class="fa-solid fa-globe"></i></label>
                                 <p id="p-person" class="title">Didáticos</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input "  type="checkbox" value="0" id="licensed" name="statusLicense">
-                                <label class="radio-label " for="licensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="licensed"><i class="fa-solid fa-landmark"></i></label>
                                 <p id="p-licensed" class="title">Clássicos</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
-                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-flag-usa"></i></label>
                                 <p id="p-notLicensed" class="title">Americana</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
-                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-heart"></i></label>
                                 <p id="p-notLicensed" class="title">Romance</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
-                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-shield-halved"></i></label>
                                 <p id="p-notLicensed" class="title">Policial</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
-                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-jedi"></i></label>
                                 <p id="p-notLicensed" class="title">Ficção</p>
                             </div>
             
                             <div class="text-center">
                                 <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
-                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <label class="radio-label " for="notLicensed"><i class="fa-regular fa-paper-plane"></i></label>
                                 <p id="p-notLicensed" class="title">Lin. Inglesa</p>
                             </div>
                         </div>
@@ -75,24 +75,114 @@
         </div>
     </div>
     
-    <div id="menu" class="menu">
+    <div class="menu">
         <nav>
-            <!-- <div id="logo">
-                <img src="/img//athenas.png" width="80px" alt="LOGO_ATENAS">
-            </div> -->
             <div class="img-aluno">
                 <img class="calc" src="img/img1.jpg" alt="IMAGEM_ALUNO">
                 <p>Nome aluno</p>
             </div>
             <div class="av-livros">
+                <!-- <hr> -->
                 <p>Livros avaliados</p>
+                <!-- <hr> -->
                 <p>Livros avaliados</p>
+                <!-- <hr> -->
                 <p>Livros avaliados</p>
-                <form action="">
-                    <button><i class="fa-solid fa-book-open"></i> Histórico</button>
-                </form>
             </div>
         </nav>
+    </div>
+
+    <div class="centralizar-livros">
+        <div class="div-livro-externo">
+            <div class="div-livro">
+                <div class="item">
+                    <img class="capa" src="/img/capa1.jpg"> 
+                    <p class="titulo">Título</p>
+                    <p class="sinopse">sinopse</p>
+                </div>
+                
+                <div class="status">
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>disponível</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>literatura americana</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+            <div class="div-livro">
+                <div class="item">
+                    <img class="capa" src="/img/capa1.jpg"> 
+                    <p class="titulo">Título</p>
+                    <p class="sinopse">sinopse</p>
+                </div>
+                
+                <div class="status">
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>disponível</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>literatura americana</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+            <div class="div-livro">
+                <div class="item">
+                    <img class="capa" src="/img/capa1.jpg"> 
+                    <p class="titulo">Título</p>
+                    <p class="sinopse">sinopse</p>
+                </div>
+                
+                <div class="status">
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>disponível</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>literatura americana</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+            <div class="div-livro">
+                <div class="item">
+                    <img class="capa" src="/img/capa1.jpg"> 
+                    <p class="titulo">Título</p>
+                    <p class="sinopse">sinopse</p>
+                </div>
+                
+                <div class="status">
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>disponível</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>literatura americana</span>
+                    </div>
+                    <div>
+                        <i class="fa fa-home"></i>
+                        <span>5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="https://kit.fontawesome.com/a9ac96b7ba.js"
