682d7c30e958025fa7829cb9bc29aa812851999d
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-11-07 17:07:10 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<div id="menu" class="menu">
+        <nav>
+            <div style="text-align: right;">
+                <button onclick="ToggleMenu()" id="btnClose">X</button>
+                <button onclick="Abrir()" id="btnOpen" style="display: none;">></button>
+            </div>
+            <ul>
+                <img id="logo" src="/img/wwAsset 6.png" width="180px" alt="LOGO">
+                <li><a class="paragrafo" href="inserirProdutos.php"><i class="fa-solid fa-cash-register"></i> Inserir Receita</a></li>
+                <li><a class="paragrafo" href="receita.php"><i class="fa-solid fa-cash-register"></i> Receita</a></li>
+                <li><a class="paragrafo" href="inserirDespesas.php"><i class="fa-solid fa-cash-register"></i> Inserir Despesas</a></li>
+                <li><a class="paragrafo" href="despesa.php"><i class="fa-solid fa-money-bills"></i> Despesas</a></li>
+                <li><a class="paragrafo" href="invest.php"><i class="fa-solid fa-money-bill"></i> Investimento</a></li>
+                <li><a class="paragrafo" href="renda.php"><i class="fa-solid fa-wallet"></i> Renda</a></li>
+                <li><a class="paragrafo" href="func.php"><i class="fa-solid fa-users"></i> Quadro de funcionários</a></li>
+                <li><a class="paragrafo" href="perfil.php"><i class="fa-regular fa-user"></i> Perfil</a></li>
+                <li><a class="paragrafo" href="index.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
+            </ul>
+        </nav>
+    </div>
---
+++ home.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home Aluno</title>
+</head>
+<body>
+    <div class="dimensao">
+        <div class="menu2">
+            <div class="central-input">
+                <input type="text" name="" id="" placeholder="Procure seu livro aqui"><i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <nav>
+                <div class='centralizar-filtro'>
+                    <div class='filtro'>
+                        <div class="fast-filter">
+                            <div class="text-center">
+                                <input class="radio-input " type="checkbox" value="0" id="company" name="type" checked>
+                                <label class="radio-label " for="company"><i class="fa-solid fa-calendar-check"></i></label>
+                                <p id="p-company" class="title">Todos</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="person" name="type">
+                                <label class="radio-label " for="person"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-person" class="title">Didáticos</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input "  type="checkbox" value="0" id="licensed" name="statusLicense">
+                                <label class="radio-label " for="licensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-licensed" class="title">Clássicos</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-notLicensed" class="title">Americana</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-notLicensed" class="title">Romance</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-notLicensed" class="title">Policial</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-notLicensed" class="title">Ficção</p>
+                            </div>
+            
+                            <div class="text-center">
+                                <input class="radio-input" type="checkbox" value="1" id="notLicensed" name="statusLicense">
+                                <label class="radio-label " for="notLicensed"><i class="fa-solid fa-calendar-days"></i></label>
+                                <p id="p-notLicensed" class="title">Lin. Inglesa</p>
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+            </nav>
+        </div>
+    </div>
+    
+    <div id="menu" class="menu">
+        <nav>
+            <!-- <div id="logo">
+                <img src="/img//athenas.png" width="80px" alt="LOGO_ATENAS">
+            </div> -->
+            <div class="img-aluno">
+                <img class="calc" src="img/img1.jpg" alt="IMAGEM_ALUNO">
+                <p>Nome aluno</p>
+            </div>
+            <div class="av-livros">
+                <p>Livros avaliados</p>
+                <p>Livros avaliados</p>
+                <p>Livros avaliados</p>
+                <form action="">
+                    <button><i class="fa-solid fa-book-open"></i> Histórico</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    <script src="https://kit.fontawesome.com/a9ac96b7ba.js"
+    crossorigin="anonymous"></script>
+</body>
+</html>
